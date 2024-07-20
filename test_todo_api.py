4cf3a05ce72d63753316a0523eab8fa860e10a55
atrox3d <atrox3d@gmail.com>
2024-07-20 08:13:49 +0200

    add references

--- test_todo_api.py
+++ test_todo_api.py
@@ -1,6 +1,8 @@
 import json
 import requests
 
+# api docs: https://todo.pixegami.io/docs
+# api code: https://github.com/pixegami/todo-list-api
 ENDPOINT = 'https://todo.pixegami.io'
 
 def test_can_call_endpoint():
