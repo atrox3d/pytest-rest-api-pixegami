d458a1db3630718cb0bfa3af49caac216ced2ce0
atrox3d <atrox3d@gmail.com>
2024-07-21 06:41:35 +0200

    test_can_create_task

--- test_todo_api.py
+++ test_todo_api.py
@@ -10,3 +10,31 @@ def test_can_call_endpoint():
     assert response.status_code == 200
 
 
+def test_can_create_task():
+    create_task_endpoint = f'{ENDPOINT}/create-task'
+    payload = {
+        "content": "test content",
+        "user_id": "test_user_a3d",
+        # "task_id": "task_id",
+        "is_done": False
+    }
+    
+    create_task_response = requests.put(create_task_endpoint, json=payload)
+    assert create_task_response.status_code == 200
+    
+    create_task_data = create_task_response.json()
+    print(create_task_data)
+    task_id = create_task_data['task']['task_id']
+
+    get_task_endpoint = f'{ENDPOINT}/get-task/{task_id}'
+    get_task_response = requests.get(get_task_endpoint)
+    assert get_task_response.status_code == 200
+
+    get_task_data = get_task_response.json()
+    print(get_task_data)
+    print(get_task_data == create_task_data['task'])
+    assert get_task_data == create_task_data['task']
+
+
+
+
