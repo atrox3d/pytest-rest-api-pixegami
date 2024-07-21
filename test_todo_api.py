f296839605d99adcefa08b142807e01113954a8b
atrox3d <atrox3d@gmail.com>
2024-07-21 06:54:50 +0200

    refactor payload, create and get task

--- test_todo_api.py
+++ test_todo_api.py
@@ -5,36 +5,42 @@ import requests
 # api code: https://github.com/pixegami/todo-list-api
 ENDPOINT = 'https://todo.pixegami.io'
 
-def test_can_call_endpoint():
-    response = requests.get(ENDPOINT)
-    assert response.status_code == 200
-
-
-def test_can_create_task():
-    create_task_endpoint = f'{ENDPOINT}/create-task'
-    payload = {
+def get_payload():
+    return {
         "content": "test content",
         "user_id": "test_user_a3d",
         # "task_id": "task_id",
         "is_done": False
     }
-    
-    create_task_response = requests.put(create_task_endpoint, json=payload)
+
+def create_task(payload):
+    endpoint = f'{ENDPOINT}/create-task'
+    response = requests.put(endpoint, json=payload)
+    return response
+
+def get_task(task_id):
+    endpoint = f'{ENDPOINT}/get-task/{task_id}'
+    response = requests.get(endpoint)
+    return response
+
+def test_can_call_endpoint():
+    response = requests.get(ENDPOINT)
+    assert response.status_code == 200
+
+def test_can_create_task():
+    create_task_response = create_task(get_payload())
     assert create_task_response.status_code == 200
     
     create_task_data = create_task_response.json()
-    print(create_task_data)
     task_id = create_task_data['task']['task_id']
 
-    get_task_endpoint = f'{ENDPOINT}/get-task/{task_id}'
-    get_task_response = requests.get(get_task_endpoint)
+    get_task_response = get_task(task_id)
     assert get_task_response.status_code == 200
 
     get_task_data = get_task_response.json()
-    print(get_task_data)
-    print(get_task_data == create_task_data['task'])
     assert get_task_data == create_task_data['task']
 
-
+def test_can_update_task():
+    ...
 
 
